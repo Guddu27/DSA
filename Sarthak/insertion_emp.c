3e9266059e929d3f36fb410ded66699f62bad5c6
Sarthak Gatkal <167743205+Guddu27@users.noreply.github.com>
2024-09-02 01:21:31 +0530

    Add files via upload

---
+++ Sarthak/insertion_emp.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+typedef struct
+{
+    char name[30];
+    int age;
+    int salary;
+} RECORD;
+RECORD emp[100];
+//------------------------------------------------------------
+int readFile(RECORD *a)
+{
+    int i = 0;
+    FILE *fp;
+    char inputfile[20];
+    printf("\n Enter Filename to Read Input Data : ");
+    scanf("%s", inputfile);
+    if ((fp = fopen(inputfile, "r")) != NULL)
+    {
+        while (!feof(fp))
+        {
+            fscanf(fp, "%s%d%d", a[i].name, &a[i].age, &a[i].salary);
+            i++;
+        }
+    }
+    return i; // number of records read
+}
+//-----------------------------------------------------
+void writeFile(RECORD *a, int n)
+{
+    int i = 0;
+    FILE *fp;
+    char outputfile[20];
+    printf("\n \n Enter Filename to write sorted data : ");
+    scanf("%s", outputfile);
+    if ((fp = fopen(outputfile, "w")) != NULL)
+    {
+        for (i = 0; i < n; i++)
+        {
+            fprintf(fp, "%s\t%d\t%d\n", a[i].name, a[i].age, a[i].salary);
+        }
+    }
+}
+//-----------------------------------------------------
+int Insertionsort(RECORD *a, int n)
+{
+    int next, i;
+    RECORD newelement;
+    for (next = 1; next < n; ++next)
+    {
+        // newelement is element to be inserted
+        newelement = a[next];
+        // shift element > newelement to right by 1 pos
+        for (i = next - 1; i >= 0 && newelement.age < a[i].age; i--)
+        {
+            a[i + 1] = a[i];
+        }
+        // insert new element at pos i+1
+        a[i + 1] = newelement;
+    }
+}
+//-----------------------------------------------
+int main()
+{
+    int n;
+    n = readFile(emp);
+    printf("\n\n ------- Data successfully Read From File------------");
+    Insertionsort(emp, n);
+    writeFile(emp, n);
+    printf("\n\n ------- Sorted Data successfully Write into Specified File------------");
+    return 0;
+}
